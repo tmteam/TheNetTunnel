0f3e8d68b9c7d5fa286fbb19040c5700015b30f9
tmteam <tmteam@yandex.ru>
2017-08-04 02:15:38 +0400

    protobuf-net package version updated

---
+++ src/TNT/Presentation/ExceptionMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TNT.Exceptions.Remote;
+
+namespace TNT.Presentation
+{
+    public class ErrorMessage
+    {
+        public ErrorMessage(short? messageId, short? askId, ErrorType type, string additionalExceptionInformation)
+        {
+            this.MessageId = messageId;
+            this.AskId = askId;
+            ErrorType = type;
+            Exception = RemoteException.Create(type, additionalExceptionInformation, messageId, askId);
+        }
+       
+        public short? MessageId { get; set; }
+        public short? AskId { get; set; }
+        public ErrorType ErrorType { get; set; }
+        public string AdditionalExceptionInformation { get; set; }
+        public  RemoteException Exception { get; }
+    }
+}
