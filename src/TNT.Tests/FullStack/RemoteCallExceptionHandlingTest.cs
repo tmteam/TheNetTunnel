9aeac4909b37065598864ce33e6859306dd404d3
tmteam <tmteam@yandex.ru>
2017-11-14 01:11:50 +0400

    [fix] Additional remote call exception information receiving

--- src/TNT.Tests/FullStack/RemoteCallExceptionHandlingTest.cs
+++ src/TNT.Tests/FullStack/RemoteCallExceptionHandlingTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
-using TNT.Api;
-using TNT.Exceptions.Local;
 using TNT.Exceptions.Remote;
 using TNT.Presentation.ReceiveDispatching;
 using TNT.Testing;
@@ -10,65 +12,53 @@ using TNT.Tests.Contracts;
 namespace TNT.Tests.FullStack
 {
     [TestFixture]
-    public class RcpCallsExceptionsTest
+    public class RemoteCallExceptionHandlingTest
     {
         [Test]
-        public void ProxyConnectionIsNotEstablishedYet_SayCallThrows()
+        public void Origin_AskExceptioanlCallback_Throws()
         {
-            var channel = new TestChannel();
+            var channelPair = TntTestHelper.CreateChannelPair();
+
             var proxyConnection = TntBuilder
                 .UseContract<ITestContract>()
                 .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channel)
+                .UseContractInitalization((c, _) =>
+                {
+                    c.OnAsk += () => { throw new InvalidOperationException(); };
+                })
+                .UseChannel(channelPair.CahnnelA)
                 .Build();
 
-            TestTools.AssertThrowsAndNotBlocks<ConnectionIsNotEstablishedYet>(() => proxyConnection.Contract.Say());
-        }
-
-
-        [Test]
-        public void ProxyConnectionIsNotEstablishedYet_AskCallThrows()
-        {
-            var channel = new TestChannel();
-            var proxyConnection = TntBuilder
-                .UseContract<ITestContract>()
+            var originConnection = TntBuilder
+                .UseContract<ITestContract, TestContractMock>()
                 .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channel)
+                .UseChannel(channelPair.ChannelB)
                 .Build();
 
-            TestTools.AssertThrowsAndNotBlocks<ConnectionIsNotEstablishedYet>(() => proxyConnection.Contract.Ask());
-        }
-
-        [Test]
-        public void ProxyConnectionIsLost_SayCallThrows()
-        {
-            var channel = new TestChannel();
-            var proxyConnection = TntBuilder
-                .UseContract<ITestContract>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channel)
-                .Build();
-            channel.ImmitateConnect();
-            channel.ImmitateDisconnect();
-            TestTools.AssertThrowsAndNotBlocks<ConnectionIsLostException>(() => proxyConnection.Contract.Say());
+            channelPair.ConnectAndStartReceiving();
+            TestTools.AssertThrows_AndNotBlocks_AndContainsInfo<RemoteUnhandledException>(() => originConnection.Contract.OnAsk());
         }
 
         [Test]
-        public void ProxyConnectionIsLost_AskCallThrows()
+        public void Proxy_AskWithException_Throws()
         {
-            var channel = new TestChannel();
+            var channelPair = TntTestHelper.CreateChannelPair();
             var proxyConnection = TntBuilder
-                .UseContract<ITestContract>()
+                .UseContract<IExceptionalContract>()
                 .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channel)
+                .UseChannel(channelPair.CahnnelA)
                 .Build();
 
-            channel.ImmitateConnect();
-            channel.ImmitateDisconnect();
+            var originConnection = TntBuilder
+                .UseContract<IExceptionalContract, ExceptionalContract>()
+                .UseReceiveDispatcher<NotThreadDispatcher>()
+                .UseChannel(channelPair.ChannelB)
+                .Build();
 
-            TestTools.AssertThrowsAndNotBlocks<ConnectionIsLostException>(() => proxyConnection.Contract.Ask());
+            channelPair.ConnectAndStartReceiving();
+            TestTools.AssertThrows_AndNotBlocks_AndContainsInfo<RemoteUnhandledException>(() => proxyConnection.Contract.Ask());
+
         }
-
 
 
         [Test]
@@ -89,8 +79,10 @@ namespace TNT.Tests.FullStack
 
             channelPair.ConnectAndStartReceiving();
 
-            TestTools.AssertThrowsAndNotBlocks<RemoteContractImplementationException>(()=> proxyConnection.Contract.Ask());
+            TestTools.AssertThrows_AndNotBlocks_AndContainsInfo<RemoteContractImplementationException>(() => proxyConnection.Contract.Ask());
         }
+
+
         [Test]
         public void Proxy_SayMissingCord_NotThrows()
         {
@@ -149,25 +141,6 @@ namespace TNT.Tests.FullStack
             channelPair.ConnectAndStartReceiving();
             TestTools.AssertNotBlocks(proxyConnection.Contract.Say);
         }
-        [Test]
-        public void Proxy_AskWithException_Throws()
-        {
-            var channelPair = TntTestHelper.CreateChannelPair();
-            var proxyConnection = TntBuilder
-                .UseContract<IExceptionalContract>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channelPair.CahnnelA)
-                .Build();
-
-            var originConnection = TntBuilder
-                .UseContract<IExceptionalContract, ExceptionalContract>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channelPair.ChannelB)
-                .Build();
-
-            channelPair.ConnectAndStartReceiving();
-            TestTools.AssertThrowsAndNotBlocks<RemoteUnhandledException>(() => proxyConnection.Contract.Ask());
-        }
 
         [Test]
         public void Origin_SayExceptioanlCallback_NotThrows()
@@ -196,30 +169,7 @@ namespace TNT.Tests.FullStack
         }
 
 
-        [Test]
-        public void Origin_AskExceptioanlCallback_Throws()
-        {
-            var channelPair = TntTestHelper.CreateChannelPair();
 
-            var proxyConnection = TntBuilder
-                .UseContract<ITestContract>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseContractInitalization((c, _) =>
-                {
-                    c.OnAsk += () => { throw new InvalidOperationException(); };
-                })
-                .UseChannel(channelPair.CahnnelA)
-                .Build();
-
-            var originConnection = TntBuilder
-                .UseContract<ITestContract, TestContractMock>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channelPair.ChannelB)
-                .Build();
-
-            channelPair.ConnectAndStartReceiving();
-            TestTools.AssertThrowsAndNotBlocks<RemoteUnhandledException>(() => originConnection.Contract.OnAsk());
-        }
         [Test]
         public void Origin_AsksNotImplemented_returnsDefault()
         {
@@ -242,38 +192,5 @@ namespace TNT.Tests.FullStack
             var answer = TestTools.AssertNotBlocks(originConnection.Contract.OnAsk).Result;
             Assert.AreEqual(default(int), answer);
         }
-
-        [Test]
-        public void Disconnected_duringOriginAsk_throws()
-        {
-            var channelPair = TntTestHelper.CreateChannelPair();
-
-            var originConnection = TntBuilder
-                .UseContract<ITestContract, TestContractMock>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseChannel(channelPair.ChannelB)
-                .Build();
-
-            var proxyConnection = TntBuilder
-                .UseContract<ITestContract>()
-                .UseReceiveDispatcher<NotThreadDispatcher>()
-                .UseContractInitalization((c, _) =>
-                {
-                    c.OnAsk += () =>
-                    {
-                        channelPair.Disconnect();
-                        try {
-                            //call say method to notify channel about disconnection (tcp channel immitation)
-                            originConnection.Contract.Say();
-                        } catch (ConnectionIsLostException) { /*suppressTheException*/ }
-                        return 0;
-                    };
-                })
-                .UseChannel(channelPair.CahnnelA)
-                .Build();
-            channelPair.ConnectAndStartReceiving();
-            TestTools.AssertThrowsAndNotBlocks<ConnectionIsLostException>(() => originConnection.Contract.OnAsk());
-        }
-      
     }
 }
--- src/TNT/Exceptions/Remote/RemoteContractImplementationException.cs
+++ src/TNT/Exceptions/Remote/RemoteContractImplementationException.cs
@@ -2,7 +2,7 @@ namespace TNT.Exceptions.Remote
 {
     public class RemoteContractImplementationException : RemoteException
     {
-        public RemoteContractImplementationException(short messageId, short? askId, bool isFatal,   string message = null) 
+        public RemoteContractImplementationException(short? messageId, short? askId, bool isFatal,   string message = null) 
             : base(ErrorType.ContractSignatureError, isFatal, messageId, askId, message)
         {
         }
